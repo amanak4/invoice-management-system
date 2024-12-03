10770e0836c3db07f44c79e4b15a8b8917650a40
amanak7 <22je0094@iitism.ac.in>
2024-12-04 03:04:36 +0530

    code str improved

--- frontend/src/base_url.js
+++ frontend/src/base_url.js
@@ -1,2 +1,2 @@
-// export const base_url="https://invoice-management-system-hd96.onrender.com/api/invoices";
-export const base_url="http://localhost:3000/api/invoices";
+export const base_url="https://invoice-management-system-hd96.onrender.com/api/invoices";
+// export const base_url="http://localhost:3000/api/invoices";
